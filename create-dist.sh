4a3c3238e718ab59a91952f16b9601281c9aa697
Jason Rouet <56646501+jaz-on@users.noreply.github.com>
2025-10-29 23:31:45 +0000

    ci: remove legacy create-dist.sh; use GitHub Actions artifact workflow

--- create-dist.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Replaced by GitHub Actions workflow .github/workflows/build-dist.yml
-echo "This script has been deprecated. Use the GitHub Actions workflow to build artifacts." >&2
-exit 1
